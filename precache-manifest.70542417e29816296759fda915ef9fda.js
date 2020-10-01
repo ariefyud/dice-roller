c48a04db2b63aa9c5a4a5caea100ad73927bca9b
ariefyud <ariefyud12@gmail.com>
2020-10-01 22:14:12 +0700

    Updates

---
+++ precache-manifest.70542417e29816296759fda915ef9fda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f899566be52112a12c39ef0c4053331b",
+    "url": "/dice-roller/index.html"
+  },
+  {
+    "revision": "1bcdcc2f716402023d20",
+    "url": "/dice-roller/static/css/main.299a75ce.chunk.css"
+  },
+  {
+    "revision": "562c9deba2e4d826e7e4",
+    "url": "/dice-roller/static/js/2.cd8d8fa3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-roller/static/js/2.cd8d8fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bcdcc2f716402023d20",
+    "url": "/dice-roller/static/js/main.49d3ea81.chunk.js"
+  },
+  {
+    "revision": "1fef78c38f42099e7f04",
+    "url": "/dice-roller/static/js/runtime-main.f9aa7fc1.js"
+  }
+]);
